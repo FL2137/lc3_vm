e3a18e61427685a9a054da5dd70ac442a0a0ff18
CoolyFooly <franek1519@gmail.com>
2026-08-11 20:05:09 +0200

    add set, reset, and check bit helpers.

--- util.h
+++ util.h
@@ -4,10 +4,38 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
-#define PRINT_BITS(T, N)                \
-        for (int i = 0; i < N; i++) {   \
-            printf("%d", (T >> i) & 1);  \
+#define PRINT_BITS(T, N)                       \
+        for (int i = (N - 1); i >= 0; i--) {   \
+            printf("%d", (T >> i) & 1);        \
         } printf("\n")
+
+
+uint8_t check_bit(uint16_t i, uint16_t nbit) {
+    return (i >> nbit) & 1;
+}
+
+uint16_t reset_bit(uint16_t i, uint16_t nbit) {
+    uint16_t x = ~(1 << nbit);
+    return (i & x);
+}
+
+uint16_t set_bit(uint16_t i, uint16_t nbit) {
+    uint16_t x = (1 << nbit);
+    return (i | x);
+}
+
+uint16_t sign_extend(uint16_t n, uint16_t sign_bit) {
+    if (((n >> sign_bit) & 1) == 1) {
+        uint16_t extended = 0xffff;
+        for (int i = 16; i > sign_bit; i--) {
+            if (check_bit(n, i) == 0) {
+                reset_bit(extended, i);
+            }
+        }
+        return extended;
+    }
+    return n;
+}
+
 
 #endif // UTIL_H
